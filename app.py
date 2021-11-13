0eb88ca5cf66ab9450c33b936ca09ff7ff7c7fda
Mariusz693 <mariush693@gmail.com>
2021-11-13 20:37:34 +0100

    Do_Index_Page

--- app.py
+++ app.py
@@ -12,10 +12,40 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+from models import Book, Client, Category, Author
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+
+@app.route("/books/")
+def books():
+    return render_template('index.html')
+
+
+@app.route("/authors/")
+def authors():
+    return render_template('index.html')
+
+
+@app.route("/clients/")
+def clients():
+    return render_template('index.html')
+
+
+@app.route("/add_author/", methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        new_author = Author(name)
+        db.session.add(new_author)
+        db.session.commit()
+        
+        return "Dodano autora"
+
+    return render_template('author_add.html')
 
 
 if __name__ == '__main__':
